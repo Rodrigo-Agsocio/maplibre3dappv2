3c12e5c83d0562315b7d790050045b46e51e86af
Rodrigo-Agsocio <agsocio.ret@gmail.com>
2025-02-07 12:51:46 -0600

    This is a working version of a map with satellite imagery, supposedly free of fees.

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
   const map = new maplibregl.Map({
     container: 'map', // container ID
-    style: 'https://demotiles.maplibre.org/style.json', // Hosted style or your style URL
+    style: {
+      "version": 8,
+      "sources": {
+        "esriWorldImagery": {
+          "type": "raster",
+          "tiles": ["https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"],
+          "tileSize": 256,
+          "attribution": "Sources: Esri, Maxar, Earthstar Geographics, CNES/Airbus DS, USDA, USGS, AeroGRID, IGN, and the GIS User Community"
+        }
+      },
+      "layers": [
+        {
+          "id": "esri-world-imagery",
+          "type": "raster",
+          "source": "esriWorldImagery",
+          "minzoom": 0,
+          "maxzoom": 22
+        }
+      ]
+    }, // Use ESRI World Imagery
     center: [-119.4179, 36.7783], // Starting position [lng, lat]
     zoom: 5.5 // Starting zoom level
   });
